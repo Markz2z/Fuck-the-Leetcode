d06b90198f720888b189df998336f7a929e9c5a1
zengmingkun <zengmingkun@xiaomi.com>
2016-01-14 20:21:17 +0800

    update2016

---
+++ 7_Reverse_Integer.c
@@ -0,0 +1,33 @@
+int getBit(int num, int* index) {
+    int count = 0;
+    while(num>0) {
+        index[count++] = num%10;
+        num /= 10;
+    }
+    return count;
+}
+
+int reverse(int x) {
+    int bit[10];
+    int is_positive = x>0? 1:0; 
+    memset(bit, 0 ,10*sizeof(int));
+    x = is_positive? x:-x;
+    int count = getBit(x, bit);
+    int i, j;
+    int res = 0;
+    int max = 2147483648;
+    if((count>10) || (count==10 && bit[0]>2)){
+        return 0;
+    }
+    for(i=count-1, j=1;i>=0;i--,j*=10){
+        res += bit[i] * j;
+        max -= bit[i] * j;
+        if(max<0){
+            return 0;
+        }
+    }
+    if(is_positive==0) {
+        res *= -1;
+    }
+    return res;
+}
