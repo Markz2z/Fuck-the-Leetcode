45c99cc63f3ecaf4de88cbbb65d92a42474d7bab
Markz2z <ustczmk@gmail.com>
2016-02-09 22:33:54 +0800

    2016_2_9

--- 28_Implement_strStr.c
+++ 28_Implement_strStr.c
@@ -1,37 +1,42 @@
-#include "stdio.h"
-
 int strStr(char* haystack, char* needle) {
-    int idx = 0;
-    char * ptr1 = haystack;
-    char * ptr2 = needle;
-    char * temp = NULL;
-    int    num_temp = 0;
+    int  idx   = 0;
+    char *ptr1 = haystack;
+    char *ptr2 = needle;
+    char *temp = NULL;
+    int  num_temp = 0;
+    int  bit[256];
+
+    if((*haystack!='\0' && *needle=='\0')||(*haystack=='\0' && *needle=='\0'))
+        return 0;
+
     while(*ptr1!='\0') {
-        idx++;
         if(*ptr2==*ptr1) {
-            printf("%c\n", *ptr1);
+            memset(bit, 0, 256*sizeof(int));
+            bit[*ptr1] = 1;
             temp = ptr1;
-            while(*ptr2==*ptr1 && *ptr2!='\0') {
+            while(*ptr2==*ptr1 && *ptr2!='\0' && *ptr1!='\0') {
+                bit[*ptr1] = 1;
                 ptr2++;
                 ptr1++;
             }
-            if(*ptr2=='\0') 
+            if(*ptr2=='\0') {
                 return idx;
+            }
+            if(*ptr1=='\0') {
+                return -1;
+            }
             else {
-                ptr1 = temp;
-                ptr2 = needle;
+                if(bit[*ptr1]==0) {
+                    ptr2 = needle;
+                    idx += ptr1 - temp;
+                }else {
+                    ptr1 = temp;
+                    ptr2 = needle;
+                }
             }
         }
+        idx++;
         ptr1++;
     }
-    if(idx==0) {
-        return -1;
-    }
-}
-
-int main() {
-    char * str1 = "abcdef";
-    char * str2 = "ef";
-    printf("%d\n",strStr(str1, str2));
-    return 1;
+    return -1;
 }
