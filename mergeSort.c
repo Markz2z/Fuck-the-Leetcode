78be1308094838d5a2c9fec0e136dc83ede70252
Markz2z <ustczmk@gmail.com>
2016-02-21 16:56:55 +0800

    2016-2-21

--- mergeSort.c
+++ mergeSort.c
@@ -1,37 +1,44 @@
 #include "stdio.h"
 
-void merge(int * unsort, int first, int mid, int last, int * sort) {
+void merge(int* nums1, int len, int* nums2) {
+    int i = 0, j = len/2, k = 0;
+    while(i<len/2 && j < len) {
+        nums2[k++] = nums1[i] < nums1[j] ? nums1[i++] : nums1[j++];
+    }
+    while(i<len/2)
+        nums2[k++] = nums1[i++];
 
-	int i = first, j = mid;
-	int k = 0;
-	while(i < mid && j < last) {
-		sort[k++] = unsort[i] < unsort[j] ? unsort[i++] : unsort[j++];
-	}
-	while(i < mid)
-		sort[k++] = unsort[i++];
-	while(j < last)
-		sort[k++] = unsort[j++];
+    while(j<len)
+        nums2[k++] = nums1[j++];
 
-	for(int v=0;v<k;v++)
-		unsort[first+v] = sort[v];
+    for(int v=0;v<k;v++) {
+        nums1[v] = nums2[v];
+    }
 }
 
-void mergeSort(int* unsort, int first, int last, int* sort) {
-	if(first + 1 < last) {
-		int mid = (first + last) / 2;
-		mergeSort(unsort, first, mid, sort);
-		mergeSort(unsort, mid, last, sort);
-		merge(unsort, first, mid, last, sort);
-	}
+// void split(int *array1, int start, int last, int *array2) {
+// 	if(start + 1 < last) {
+// 		int mid = (start + last) / 2;
+// 		split(array1, start, mid, array2);
+// 		split(array1, mid, last, array2);
+// 		merge(array1, start, mid, last, array2);
+// 	}
+// }
+
+void split(int *array1, int len, int *array2) {
+    if(len>1) {
+        split(array1, len/2, array2);
+        split(array1+len/2, len - len/2, array2);
+        merge(array1, len, array2);
+    }
 }
 
 int main() {
-	int num1[10] = {1,3,10,2,4,7,6,5,9,8};
-	int * num3 = (int *)malloc(sizeof(int) * 10);
-	mergeSort(num1, 0, 10, num3);
-	for(int i=0;i<10;i++) {
-		printf("%d ", num3[i]);
-	}
-	printf("\n");
-	return 1;
+    int num[11] = {11,1,9,8,7,6,5,2,3,4,10};
+    int num2[11];
+    split(num, 11, num2);
+    for(int i=0;i<11;i++)
+        printf("%d ", num[i]);
+    printf("\n");
+    return 1;
 }
