798e04a30f488a6a7abfffd449a4a290c07cdeb8
Markz2z <ustczmk@gmail.com>
2016-03-27 23:55:13 +0800

    2016.3.27

---
+++ 338_Counting_Bits.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> result;
+        if(num>=0) {
+        	int count[num+1];
+        	int i = 0;
+        	while(i<=num) {
+        		if(i==0){
+        			count[i] = 0;
+        			result.push_back(i);
+        		}else if(i==1) {
+        			count[i] = 1;
+        			result.push_back(i);
+        		}else {
+        			if(i%2) {
+        				count[i] = count[i>>1]+1;
+        				result.push_back(count[i>>1]+1);
+        			}else {
+        				count[i] = count[i>>1];
+        				result.push_back(count[i>>1]);
+        			}
+        		}
+        		++i;
+        	}
+        }
+        return result;
+    }
+};
