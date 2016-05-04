8b37eed663304782fcf0d1fdb921a9938ff1b7b8
Markz2z <ustczmk@gmail.com>
2016-05-04 14:00:11 +0800

    update

---
+++ producer_consumer.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+import "time"
+
+func Producer (queue chan<- int){
+        for i:= 0; i < 10; i++ {
+                fmt.Println("produce:", i)
+                queue <- i
+        }
+}
+
+func Consumer( queue <-chan int){
+        for i :=0; i < 10; i++{
+                v := <- queue
+                fmt.Println("receive:", v)
+        }
+}
+
+func main(){
+        queue := make(chan int, 1)
+        go Producer(queue)
+        go Consumer(queue)
+        time.Sleep(1e9)
+}
