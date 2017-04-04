3999036a3a335dd9aa0e560e70f9704e327daa98
Markz2z <ustczmk@gmail.com>
2017-04-04 17:02:31 +0800

    add 540

---
+++ 540-single-element-in-a-sorted-array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def singleNonDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
